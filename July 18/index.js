515ac9edbc342e84b6b820beb2d0442ff80d2e06
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-07-18 16:04:47 +0530

    JUly 18 done

---
+++ July 18/index.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+
+const Roronoa = require("./object");
+console.log("This is Gaurav's Address : ",Roronoa);
+
---
+++ July 18/object.js
@@ -0,0 +1,8 @@
+let obj = {
+    name:"Gaurav",
+    middleName:"Kumar",
+    lastname:"Thakur",
+    developer:true
+}
+
+module.exports = obj;
